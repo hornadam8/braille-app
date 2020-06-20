1142d0b68a49ac003bb4dfa15be02e208521c879
hornadam8 <hornadam8@gmail.com>
2020-06-20 17:20:55 -0600

    fixed Cohort model

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,3 @@
-Teacher/Student models for associations
+
 login form
 add cohorts button for teachers, view cohorts for all 
--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,1 +1,4 @@
 class Cohort < ApplicationRecord
+    belongs_to :teacher, class_name "User"
+    has_many :students, class_name "User"
+end
